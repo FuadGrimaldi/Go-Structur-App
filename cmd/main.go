5b27034fa0f24ef0f0fe643aac2a5f6c997bdae5
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-08-02 19:12:52 +0700

    feat: add database connection

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,29 @@ package main
 import (
 	"fmt"
 	"go-app/config"
+	"go-app/pkg/database"
 )
+
+type User struct {
+	ID int64
+	Name string
+	Address string
+	Gender string
+	Email string
+}
 
 func main() {
 	cfg, err := config.NewConfig(".env")
 	checkError(err)
 
+	db, err := database.ConnectToPostgres(cfg)
+	checkError(err)
+
+	users := make([]User, 0)
+	err = db.Table("user_tb").Find(&users).Error
+	checkError(err)
+
+	fmt.Println(users)
 	fmt.Println(cfg)
 }
 
